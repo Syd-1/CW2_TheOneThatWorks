52fb8f79c0069df90c2e7ebb7ad9e328e32b4945
SydColvin <57715986+SydColvin@users.noreply.github.com>
2019-11-15 15:11:58 +0000

    Update SHCCompetitor.java

--- SHCCompetitor.java
+++ SHCCompetitor.java
@@ -40,21 +40,5 @@ public class SHCCompetitor extends Competitor{
 		for (int index = 0; index < scores.length(); index++) {
 			sumScore += sA[index];}				
 		return (double) Math.round(sumScore*(11-level)*5/20)/10; 		
-	}
-	
-	//Calculate score array
-//	public int[] scoreArray() {
-//		int[] sA = new int[scores.length()];
-//		for (int i = 0; i < scores.length(); i++) {
-//			sA[i] = Character.getNumericValue(scores.charAt(i));
-//			}
-//		return sA;		
-//	}
-//	//Score array in report format
-//	public String getScoreArray() {
-//	return Arrays.toString(scoreArray()).replaceFirst("]", "").replace("[", "");}
-	
-
-
-	
+	}		
 }
