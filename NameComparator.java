b6327a0047f7a67cf50dc292d404c595f3649e3f
st148 <57718210+st148@users.noreply.github.com>
2019-12-02 18:20:36 +0000

    Add files via upload

---
+++ NameComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+
+public class NameComparator implements Comparator<Competitor>
+{
+	public int compare(Competitor c1, Competitor c2) {
+		return c1.getCompName().compareTo(c2.getCompName());
+	}
+}
