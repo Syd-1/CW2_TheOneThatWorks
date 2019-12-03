2668ff1f85e969b0f872e35436918aef4f6609cc
st148 <57718210+st148@users.noreply.github.com>
2019-12-03 18:56:16 +0000

    Update main2.java

--- main2.java
+++ main2.java
@@ -1,5 +1,18 @@
-
+/**
+ * Software Engineering Foundations Assignment 2 <p>
+ *
+ * @author Group 22
+ * 
+ */
 public class main2 {
+	/**
+	 * Main Class <p>
+	 * Run the method of manager
+	 *
+	 * @author Group 22
+	 * 
+	 */
+	
 	
 	public static void main(String[] args) {
 		manager m = new manager();
