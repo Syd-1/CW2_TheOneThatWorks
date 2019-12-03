0c8b455ec00bc384854280e8d17b96f3ab3136fe
st148 <57718210+st148@users.noreply.github.com>
2019-12-03 17:12:00 +0000

    Update main2.java

--- main2.java
+++ main2.java
@@ -4,10 +4,8 @@ public class main2 {
 	public static void main(String[] args) {
 		manager m = new manager();
 		m.run();
-//	System.out.println(c2.getTableOfCompetitors());
-//	System.out.println(c2.getCompetitorReport());
 
 	}			
-	//c2.writeToFile("CompetitorsOut.txt", report);
+
 
 }
